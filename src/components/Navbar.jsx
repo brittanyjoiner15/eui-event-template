08ef2d0f6f78639fe5f0102b4f370c9329abed2e
camillet16 <camille.tindogan@canonical.com>
2022-09-28 10:16:48 +0300

    Replace Icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { EuiPageHeader } from "@elastic/eui";
 import { addCalButtons } from "../utilities/addCalButtons";
 /*import { makeRainbowText } from "../utilities/rainbowLetters";*/
-import { sessionOne, sessionTwo, rainbowCluster } from "../data/consts";
+import { sessionOne, sessionTwo, eventIcon } from "../data/consts";
 
 export default function Navbar(props) {
   return (
     <>
       <EuiPageHeader
         restrictWidth
-        iconType={rainbowCluster}
+        iconType={eventIcon}
         iconProps={{
           onClick: props.onLogoClick,
         }}

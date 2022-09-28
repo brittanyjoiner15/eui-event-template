9a55b3b59ed26a2a8eeb8428c682812f58355e42
camillet16 <camille.tindogan@canonical.com>
2022-09-28 10:02:42 +0300

    Replace NavBar Text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { EuiPageHeader } from "@elastic/eui";
 import { addCalButtons } from "../utilities/addCalButtons";
-import { makeRainbowText } from "../utilities/rainbowLetters";
+/*import { makeRainbowText } from "../utilities/rainbowLetters";*/
 import { sessionOne, sessionTwo, rainbowCluster } from "../data/consts";
 
 export default function Navbar(props) {
@@ -13,7 +13,7 @@ export default function Navbar(props) {
         iconProps={{
           onClick: props.onLogoClick,
         }}
-        pageTitle={makeRainbowText()}
+        pageTitle="IDT Conference"
         rightSideItems={[
           addCalButtons(sessionTwo.dateAndTime, sessionTwo.calendarLink),
           addCalButtons(sessionOne.dateAndTime, sessionOne.calendarLink),

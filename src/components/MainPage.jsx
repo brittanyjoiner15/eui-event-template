6d54a336ff87857da07e90c90d42c17e7313f927
iFrogHop2Worlds <bbest12g@gmail.com>
2022-10-01 10:47:45 -0700

    removed comments

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -16,11 +16,7 @@ import { Routes, Route } from 'react-router-dom'
 import history from "../utilities/history";
 
 function MainPage() {
-  // const [selectedTab, setSelectedTab] = useState("event");   // commented out because approach doesnt work with routing
 
-  // const onSelectedTabChanged = (id) => { // commented out because approache doesnt work with routing
-  //   setSelectedTab(id);
-  // };
 
   const tabs = [
     {
@@ -40,17 +36,11 @@ function MainPage() {
     },
   ];
 
-  
-  // const onLogoClick = useCallback(() => {  // commented out because it doesnt seem to work and conflicts with with routing 
-  //   onSelectedTabChanged("event");         // we have a the useEffect hook running to attach an eventlistener and handle icon clicks in the App.js
-  // }, []);
-
   return (
     <EuiPage paddingSize="none">
       <EuiFlexGroup className="eui-fullHeight">
         <EuiPageBody panelled>
         <Navbar tabs={tabs} /> 
-        {/* <Navbar tabs={tabs} onLogoClick={onLogoClick} /> */} 
           <EuiPageContent
             hasBorder={false}
             hasShadow={false}

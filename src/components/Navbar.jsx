77bd8e7b1ec2b8c46b582f8bc3c0ff7692b7151a
Brandon <brandon.cheung.dev@gmail.com>
2022-10-07 12:47:51 -0400

    Add navbar brand text routing and hover cursor (#71)
    * Update Navbar.jsx
    
    * add maxWidth to reduce brand area to click
    
    Co-authored-by: Brandon <aloysiusbrandon.cheung@mail.utoronto.ca>
    Co-authored-by: import-turtle <104483060+import-turtle@users.noreply.github.com>
    Co-authored-by: Brittany Joiner <brittanyjoiner15@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { EuiIcon, EuiPageHeader } from "@elastic/eui";
 import { addCalButtons } from "../utilities/addCalButtons";
 /*import { makeRainbowText } from "../utilities/rainbowLetters";*/
 import { sessionOne, sessionTwo, eventIcon } from "../data/consts";
+import history from "../utilities/history";
 import { useState } from "react";
 
 export default function Navbar(props) {
@@ -20,6 +21,17 @@ export default function Navbar(props) {
           onClick: props.onLogoClick,
         }}
         pageTitle="IDT Conference"
+        pageTitleProps={{
+          css: {
+            cursor: "pointer",
+            userSelect: "none",
+            maxWidth: "310px",
+          },
+          onClick: () => {
+            history.push("/");
+            window.location.reload();
+          },
+        }}
         rightSideItems={[
           addCalButtons(sessionTwo.dateAndTime, sessionTwo.calendarLink),
           addCalButtons(sessionOne.dateAndTime, sessionOne.calendarLink),
